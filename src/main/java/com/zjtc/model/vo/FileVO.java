c64b5540ab1e7301106a9b464b6d5f3126605206
lianghao <lianghao>
2021-02-18 09:18:30 +0800

    feat:添加附件VO

---
+++ src/main/java/com/zjtc/model/vo/FileVO.java
@@ -0,0 +1,35 @@
+package com.zjtc.model.vo;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@ApiModel("审核相关附件VO")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class FileVO extends Model<FileVO>{
+
+    @ApiModelProperty("附件表id")
+    private String id;
+
+    @ApiModelProperty("文件名称")
+    private String fileName;
+
+    @ApiModelProperty("附件路径")
+    private String filePath;
+
+    @ApiModelProperty("是否删除")
+    private String deleted;
+
+	@Override
+	protected Serializable pkVal() {
+		return this.id;
+	}
+	
+}
