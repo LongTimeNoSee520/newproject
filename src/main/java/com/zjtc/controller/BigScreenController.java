b978624f5eac684dd0a69aae23727c3d3c87aaaf
yuyantian <1184477297@qq.com>
2021-04-09 11:12:56 +0800

    feat:[业务系统][数字大屏] 新建controller

---
+++ src/main/java/com/zjtc/controller/BigScreenController.java
@@ -0,0 +1,22 @@
+package com.zjtc.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yuyantian
+ * @date 2021/4/9
+ * @description
+ */
+@Api(tags = "数字大屏")
+@RestController
+@RequestMapping("bigScreen/")
+@Slf4j
+public class BigScreenController {
+  //中间地图区域标点
+
+  //取水单位信息
+
+}
