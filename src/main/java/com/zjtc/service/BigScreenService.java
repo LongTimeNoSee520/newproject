6d3defeb57ed92a89d639c5eda91ca5d238f96bf
yuyantian <1184477297@qq.com>
2021-04-09 11:16:12 +0800

    feat:[业务系统][数字大屏] 新建service

---
+++ src/main/java/com/zjtc/service/BigScreenService.java
@@ -0,0 +1,10 @@
+package com.zjtc.service;
+
+/**
+ * @author yuyantian
+ * @date 2021/4/9
+ * @description
+ */
+public interface BigScreenService  {
+
+}
---
+++ src/main/java/com/zjtc/service/impl/BigScreenServiceImpl.java
@@ -0,0 +1,13 @@
+package com.zjtc.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author yuyantian
+ * @date 2021/4/9
+ * @description
+ */
+@Service
+public class BigScreenServiceImpl {
+
+}
